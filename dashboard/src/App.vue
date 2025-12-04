16d856d2522a9145d9ea105b889839c243311378
Lewenei <brianngetich041@gmail.com>
2025-12-04 15:52:52 +0300

    fix: change from local to production

--- dashboard/src/App.vue
+++ dashboard/src/App.vue
@@ -3,7 +3,7 @@ import { ref, onMounted } from 'vue'
 import axios from 'axios'
 import Chart from 'chart.js/auto'
 
-const API_URL = 'http://localhost:8000/api'
+const API_URL = 'https://mediflow-hospital-analytics-system.onrender.com/api'
 
 const stats = ref({
   today: 0,
